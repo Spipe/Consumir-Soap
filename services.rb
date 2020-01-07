f99b57001edcc98ee523646f5b46f13ba5ca47e0
Juan Avendaño Nuñez <javendano.n@gmail.com>
2020-01-07 16:22:44 -0300

    fixed bug propercase

--- services.rb
+++ services.rb
@@ -44,14 +44,29 @@ def properCase(api,client)
     @nombres = gets.chomp
     puts 'Ingrese el género (M/F): '
     @gender = gets.chomp
-
+    @genders = [
+        'M',
+        'm',
+        'F',
+        'f'
+    ]
+    if (!(@genders.include? @gender))
+        validate = 1
+        while(validate==1)
+            puts "Ingrese el género correctamente: "
+            @gender = gets.chomp
+            if ((@genders.include? @gender))
+                validate=0
+            end
+        end
+    end
     if @api=='REST'
         data = HTTParty.post(@base_uri_rest + 'nombrePropio',
             body:{
                 name: @nombres,
                 apellido_m: @materno,
                 apellido_p: @paterno,
-                gender: @gender
+                gender: @gender.capitalize()
             }   
         )
     puts data
